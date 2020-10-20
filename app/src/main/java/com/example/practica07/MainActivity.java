30b2538a88168c45daaa9ccd0185f758af1b614b
saul <saul.rodriguez@cceo.com.mx>
2020-10-20 00:15:13 -0500

    part 2 practice implementation

--- app/src/main/java/com/example/practica07/MainActivity.java
+++ app/src/main/java/com/example/practica07/MainActivity.java
@@ -6,21 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button boton;
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         boton = (Button) findViewById(R.id.boton);
         boton.setOnClickListener(this);
+        editText = (EditText) findViewById(R.id.editText);
     }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.boton) {
+            String dato = editText.getText().toString();
             Intent next = new Intent(MainActivity.this, SecondActivity.class);
+            next.putExtra("MENSAJE", dato);
             startActivity(next);
         }
     }
--- app/src/main/java/com/example/practica07/SecondActivity.java
+++ app/src/main/java/com/example/practica07/SecondActivity.java
@@ -2,13 +2,22 @@ package com.example.practica07;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    TextView texto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        texto = (TextView) findViewById(R.id.texto);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if(extras!=null){
+            String dato = extras.getString("MENSAJE");
+            texto.setText(dato);
+        }
     }
 }
